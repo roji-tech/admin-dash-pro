3119f1a1546b153f79d5e85bd94c0086671601b9
RojiTech Global <rojitech9@gmail.com>
2022-05-10 06:49:46 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Admin from "./components/AdminHome/AdminHome";
+
+function App() {
+  return (
+    <div className="App">
+      <AdminHome />
+    </div>
+  );
+}
+
+export default App;
