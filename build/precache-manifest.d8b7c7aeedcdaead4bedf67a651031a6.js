9920327c06386ddb7392eb61b8e228b6be23c487
RojiTech Global <rojitech9@gmail.com>
2022-05-19 12:14:58 +0100

    init7

---
+++ build/precache-manifest.d8b7c7aeedcdaead4bedf67a651031a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c64a11901346b8233a4d",
+    "url": "/sh-admin-dash-pro2/static/css/main.6169fee2.chunk.css"
+  },
+  {
+    "revision": "c64a11901346b8233a4d",
+    "url": "/sh-admin-dash-pro2/static/js/main.c64a1190.chunk.js"
+  },
+  {
+    "revision": "ff1e7aaadcd4bbbfe6ad",
+    "url": "/sh-admin-dash-pro2/static/js/1.ff1e7aaa.chunk.js"
+  },
+  {
+    "revision": "5affd99c11d4ecc0fa90",
+    "url": "/sh-admin-dash-pro2/static/js/runtime~main.5affd99c.js"
+  },
+  {
+    "revision": "e8a40bd3ad0e5bfdf085b33e4fc8d2b6",
+    "url": "/sh-admin-dash-pro2/index.html"
+  }
+];
