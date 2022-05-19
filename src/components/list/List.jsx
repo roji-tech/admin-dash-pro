7cd2eb450c995b4b77a6ea68bfc0420557d02438
RojiTech Global <rojitech9@gmail.com>
2022-05-19 11:20:21 +0100

    init7

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import "./list.css";
-import Datatable from "../../addOns/datatable/Datatable";
+// import Datatable from "../../addOns/datatable/Datatable";
 import fetchData from "../../DataFetch";
 
 const List = () => {
-  let url = "";
+  const [users, setUsers] = useState(null);
+
+  const location = useLocation();
+
+  let url = "https://fakerapi.it/api/v1/persons?_quantity=15";
 
   useEffect(() => {
-    fetchData(url);
+    const response = fetchData(url);
+    response.then((response) => {
+      setUsers(response);
+      console.log(response);
+    });
+    // setUsers(response);
   }, [url]);
+
   return (
     <div className="list">
       <div className="listContainer">
-        <Datatable /> list
+        <pre>
+          <code>{JSON.stringify(users, null, 4)}</code>
+        </pre>
+        {/* <Datatable />  */}list
       </div>
     </div>
   );
