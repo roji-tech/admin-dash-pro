c5ba34a97252cb744a5be3a079113cc428dc1bcf
RojiTech Global <rojitech9@gmail.com>
2022-05-24 19:26:16 +0100

    init13

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { GrUserAdmin, GrShieldSecurity } from "react-icons/gr";
+import LockIcon from "@mui/icons-material/Lock";
+import PersonIcon from "@mui/icons-material/Person";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import { useAuth } from "../Auth";
 
 import "./login.css";
@@ -37,27 +41,41 @@ const Login = () => {
     <div className="login-wrap">
       <div className="loginContainer">
         <div className="login">
+          <div className="login-header">
+            <AdminPanelSettingsIcon className="admin-icon" />
+            <h1>Admin Login</h1>
+          </div>
           <form method="post">
             <div className="formInput">
               <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                ref={emailRef}
-                required
-                name="email"
-                id="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
+              <div className="input-wrap">
+                <input
+                  type="email"
+                  ref={emailRef}
+                  required
+                  name="email"
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <PersonIcon
+                  className="auth-icon"
+                  htmlColor="red"
+                  color="blue"
+                />
+              </div>
             </div>
             <div className="formInput">
               <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                ref={passwordRef}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+              <div className="input-wrap">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  ref={passwordRef}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <LockIcon className="auth-icon" htmlColor="red" color="blue" />
+              </div>
             </div>
             <div className="formBtn">
               <button type="submit" className="gradBtn-1" onClick={handleLogin}>
@@ -70,10 +88,10 @@ const Login = () => {
           </form>
           <hr />
           <p>
-            Click here to create a
+            Click here to create a{" "}
             <span>
-              {/* <Link> */}
-              new account
+              {" "}
+              {/* <Link> */} new account
               {/* </Link> */}
             </span>
           </p>
